b98af4b63c6cb6609b5df99a1a1d303cc53cc310
portolan75 <72312211+portolan75@users.noreply.github.com>
2021-01-22 17:13:58 +0100

    added verbatimTextOutputs

--- tyt/app.R
+++ tyt/app.R
@@ -9,13 +9,25 @@ ui <- fluidPage(
       "Entertainment", "Sleeping"
     )
   ),
+  verbatimTextOutput("std_selected"),
   textInput(
     inputId = "oth_tags",
-    label = "Do you have specific activities/tag(s) you like to track time of?\
+    label = "Do you have specific activities/tag(s) you like to track time of?
     (Please list them below, each time you hit ENTER a new tag is created)",
     placeholder = "Volunteering, Holidays, Administration, etc."
-  )
+  ),
+  verbatimTextOutput("oth_selected")
 )
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  #tags_list <- reactive({
+  #  get(input$std_tags)
+  #})
+  #output$std_selected <- renderPrint({
+  #  get(input$std_tags)
+  #})
+  #output$std_selected <- renderPrint({
+  #  tags_list()
+  #})
+}
 shinyApp(ui, server)
