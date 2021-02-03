c9403cfe81e64e61725bac5f9247803a4f0d0b54
portolan75 <72312211+portolan75@users.noreply.github.com>
2021-02-03 19:53:00 +0100

    changed layout with only tabs and fluidrows

--- tyt/app.R
+++ tyt/app.R
@@ -6,22 +6,39 @@ ui <- fluidPage(
   
   # App title ----
   titlePanel("TYT- Take Your Time"),
-  
-  checkboxGroupInput(
-    inputId = "std_tags",
-    label = "Which activities/tag(s) would you like to track time of?",
-    choices = list(
-      "Eating and Cooking", "Work", "Social-Network", "Social-Out", "Sport", 
-      "Entertainment", "Sleeping"
-    )
-  ),
-  textInput(
-    inputId = "oth_tags",
-    label = "Do you have specific activities/tag(s) you like to track time of?
-    (Please list them below, each time you hit ENTER a new tag is created)",
-    placeholder = "Volunteering, Holidays, Administration, etc."
-  ),
-  textOutput("tags_selected")
+    
+  # Set the main tabsets for both input and output ----
+  tabsetPanel(type = "tabs",
+    # Input: Tab with input data (input tags, input time) ----
+    tabPanel("Create/Edit Tags", fluid = TRUE,
+      # Fluidrow layout for the first input tabset----
+      fluidRow(
+        # Sidebar panel for inputs ----
+        column(6,
+          checkboxGroupInput(
+            inputId = "std_tags",
+            label = "Which activities/tag(s) would you like to track time of?",
+            choices = list(
+              "Eating and Cooking", "Work", "Social-Network", "Social-Out", 
+              "Sport", "Entertainment", "Sleeping"
+            )
+          )
+        ),
+        br(),
+        column(6, 
+          textInput(
+            inputId = "oth_tags",
+            label = "Do you have specific activities/tag(s) you like to track time of?
+            (Please list them below, each time you hit ENTER a new tag is created)",
+            placeholder = "Volunteering, Holidays, Administration, etc."
+          )
+        ),
+        # Main panel for displaying outputs ----
+        mainPanel(tabPanel("Create/Edit Tags", textOutput("tags_selected")))
+      )
+    ),
+    # Output: Tabset w/ plot, summary, and table ----
+    tabPanel("Insert Time", tableOutput("time_table")))
 )
 
 server <- function(input, output, session) {
